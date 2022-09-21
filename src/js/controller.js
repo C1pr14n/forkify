29819ba09d941c54f8d530be49fed02e59257a6f
GeorgeCiprian <cipriant95@gmail.com>
2022-09-21 16:03:42 +0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -140,6 +140,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the app!');
+};
+
 // Publisher-Subcriber method
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -149,7 +153,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
 
